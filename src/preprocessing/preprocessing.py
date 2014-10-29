96dfcf25805d913988d2e965a346d0ba10832fc8
Edward Newell <edward.newell@gmail.com>
2014-10-28 22:05:59 -0400

    write sift_feature files on per-image basis

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -9,20 +9,25 @@ import copy
 import time
 from collections import Counter, defaultdict
 from scipy.cluster import vq
-from sklearn.cluster import KMeans
+
+try:
+	from sklearn.cluster import KMeans
+except ImportError:
+	print 'sklearn is not currently available'
 
 
 class FeatureExtractor(object):
 
-	NUM_TRAINING_IMAGES = 500
-	NUM_TEST_IMAGES = 200
+	NUM_TRAINING_IMAGES = 50000
+	NUM_TEST_IMAGES = 20000
 
 	RAW_DATA_DIR = 'data/raw'
-	RAW_IMG_DIR = 'train_images'
+	TRAIN_IMAGE_DIR = 'train_images'
+	TEST_IMAGE_DIR = 'train_images'
 	RAW_LABELS_FNAME = 'data_and_scripts/train_outputs.csv'
 	PROCESSED_DIR = 'data/processed'
 	SIFT_WORDS_FNAME = 'sift_words.json'
-	SIFT_FEATURES_FNAME = 'sift_%s.json'
+	SIFT_FEATURES_DIR = 'sift_features'
 	SIFT_VOCAB_FNAME = 'sift_vocab.json'
 
 	def __init__(
@@ -40,25 +45,81 @@ class FeatureExtractor(object):
 
 		# read all of the training outputs
 		self.read_outputs()
-		self.inputs_dir = os.path.join(self.RAW_DATA_DIR, self.RAW_IMG_DIR)
+		self.train_img_dir=os.path.join(self.RAW_DATA_DIR,self.TRAIN_IMAGE_DIR)
+		self.test_img_dir = os.path.join(self.RAW_DATA_DIR,self.TEST_IMAGE_DIR)
+ 
 
 
-	def compute_sift(self, idx):
+	def compute_or_load_sift(self, data_part, idx):
 
-		fname = os.path.join(self.inputs_dir, '%d.png' % idx)
-		image = cv2.imread(fname)
+		# first we try to load from file
+		sift_fname = self.get_sift_fname(data_part, idx)
+		if os.path.isfile(sift_fname):
+			return self.read_sift(sift_fname)
+
+		# otherwise try to calculate
+		image_fname = self.get_image_fname(data_part, idx)
+		image = cv2.imread(image_fname)
 
 		if self.sifter is None:
 			self.sifter = cv2.SIFT()
 			
 		kp, des = self.sifter.detectAndCompute(image, None)
+
+		# write the feature to disk for later
+		self.write_sift(sift_fname, des)
+
 		return des
 
 
-	def compute_all_sift_features(self):
+	def read_sift(self, fname):
+		'''
+			reads in the sift features and converts them into numpy arrays.
+		'''
+		fh = open(fname)
+		return np.load(fh)
 
+
+	def write_sift(self, fname, des):
+
+		fh = open(fname, 'w')
+		np.save(fh, des)
+		return True
+
+
+	def get_image_fname(self, data_part, idx):
+
+		fname = '%d.png' % idx
+		if data_part == 'test':
+			return os.path.join(self.test_img_dir, fname)
+		elif data_part == 'train':
+			return os.path.join(self.train_img_dir, fname)
+
+		raise ValueError('data_part must be `test` or `train`.')
+			
+
+	def get_sift_key(self, data_part, idx):
+		return '%s_%d' % (data_part, idx)
+
+
+	def get_sift_fname(self, data_part, idx):
+		'''
+			get a string that is likely to be unique and that depends on the
+			particular images that have been read
+		'''
+		fname = '%s.np' % self.get_sift_key(data_part, idx)
+		fpath = os.path.join(self.PROCESSED_DIR, self.SIFT_FEATURES_DIR, fname)
+		return fpath
+
+
+	def get_all_sift_features(self, use_cache=True):
+
+		# check if we have the sift features in memory already
+		if self.sift_features is not None and use_cache:
+			return self.sift_features
+
+		# otherwise, compute or load each sift image's sift features
 		print 'computing all sift features from scratch'
-
 		self.sift_features = {}
 		reading_list = [
 			('train', self.train_image_idxs),
@@ -76,59 +137,11 @@ class FeatureExtractor(object):
 						progress += self.NUM_TRAINING_IMAGES 
 					print '%2.1f%%' % (100 * progress / num_to_read)
 
-				key = '%s_%d' % (data_part, idx)
-				self.sift_features[key] = self.compute_sift(idx)
-
-		return self.sift_features
-
-
-	def get_sift_fname(self):
-		'''
-			get a string that is likely to be unique and that depends on the
-			particular images that have been read
-		'''
-		contents = repr(self.NUM_TRAINING_IMAGES) + repr(self.NUM_TEST_IMAGES)
-		hsh =  hashlib.sha224(contents).hexdigest()[:10]
-		return self.SIFT_FEATURES_FNAME % hsh
-
-
-	def read_sift_features(self):
-		'''
-			reads in the sift features and converts them into numpy arrays.
-		'''
-
-		print 'reading precomputed sift features from file...'
-		precomputed_sift_fname = os.path.join(
-			self.PROCESSED_DIR, self.get_sift_fname())
-		
-		self.sift_features =  json.loads(open(precomputed_sift_fname).read())
-
-		# convert the sift feature vectors to np arrays
-		for key in self.sift_features:
-			self.sift_features[key] = np.array(self.sift_features[key])
-
-		return self.sift_features
-
-
-	def get_all_sift_features(self, use_cache=True):
-
-		# check if we have the sift features in memory already
-		if self.sift_features is not None and use_cache:
-			return self.sift_features
-
-		# otherwise, look for precomputed sift features on disk
-		precomputed_sift_fname = os.path.join(
-			self.PROCESSED_DIR, self.get_sift_fname())
-
-		if os.path.isfile(precomputed_sift_fname) and use_cache:
-			self.sift_features = self.read_sift_features()
-			return self.sift_features
-
-
-		# as a last resort, compute them from scratch.  cache them.
-		self.sift_features = self.compute_all_sift_features()
-		fh = open(precomputed_sift_fname, 'w')
-		fh.write(json.dumps(self.sift_features))
+				# we adopt a naming convention to index sift features in 
+				# memory and on disk
+				key = self.get_sift_key(data_part, idx)
+				self.sift_features[key] = self.compute_or_load_sift(
+					data_part, idx)
 
 		return self.sift_features
 
